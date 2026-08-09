36de9dc6dc92ea98d7fff9d8b98bfa9a618088ea
likemike300081222-web <likemike300081222@gmail.com>
2026-08-09 07:48:05 +0200

    Update FarmChunkGenerator.java

--- src/main/java/com/mike/farmworld/generator/FarmChunkGenerator.java
+++ src/main/java/com/mike/farmworld/generator/FarmChunkGenerator.java
@@ -1,88 +1,49 @@
 package com.mike.farmworld.generator;
 
+import org.bukkit.Chunk;
 import org.bukkit.Material;
-import org.bukkit.generator.BlockPopulator;
-import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.generator.WorldInfo;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.loot.LootTables;
 
-import java.util.List;
 import java.util.Random;
 
-public class FarmChunkGenerator extends ChunkGenerator {
+/**
+ * Wird nur für Welten registriert, die mit FarmChunkGenerator erzeugt wurden.
+ * Konfiguriert frisch generierte Spawner (Mob-Typ) und Truhen (Loot-Table),
+ * da das beim reinen Blocksetzen im Generator/Populator nicht möglich ist.
+ */
+public class DungeonSetupListener implements Listener {
 
-    // Höhen relativ zur Weltbasis (funktioniert für 1.18+ mit negativer Höhe)
-    private static final int STONE_TOP_Y = 60;
-    private static final int SUBSURFACE_TOP_Y = 63;
-    private static final int SURFACE_Y = 64;
+    private static final EntityType[] HOSTILE_MOBS = {
+            EntityType.ZOMBIE, EntityType.SKELETON, EntityType.SPIDER
+    };
+    private final Random random = new Random();
 
-    private final int biomeZoneSize;
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+        if (!event.isNewChunk()) return;
+        Chunk chunk = event.getChunk();
 
-    public FarmChunkGenerator(int biomeZoneSize) {
-        this.biomeZoneSize = biomeZoneSize;
-    }
-
-    @Override
-    public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random,
-                                 int chunkX, int chunkZ, @NotNull ChunkData chunkData) {
-        int minY = worldInfo.getMinHeight();
-
-        // Bedrock
-        for (int x = 0; x < 16; x++) {
-            for (int z = 0; z < 16; z++) {
-                chunkData.setBlock(x, minY, z, Material.BEDROCK);
-
-                // Stein-Unterbau
-                for (int y = minY + 1; y <= STONE_TOP_Y; y++) {
-                    chunkData.setBlock(x, y, z, Material.STONE);
+        for (org.bukkit.block.BlockState state : chunk.getTileEntities()) {
+            if (state instanceof CreatureSpawner spawner
+                    && spawner.getSpawnedType() == null) {
+                EntityType type = HOSTILE_MOBS[random.nextInt(HOSTILE_MOBS.length)];
+                spawner.setSpawnedType(type);
+                spawner.update(true, false);
+            } else if (state instanceof Chest chest) {
+                Block block = chest.getBlock();
+                if (block.getType() == Material.CHEST) {
+                    chest.getBlockInventory().clear();
+                    chest.setLootTable(LootTables.SIMPLE_DUNGEON.getLootTable());
+                    chest.update(true, false);
                 }
-
-                int worldX = (chunkX << 4) + x;
-                int worldZ = (chunkZ << 4) + z;
-                FarmBiome biome = FarmBiome.forColumn(worldX, worldZ, biomeZoneSize);
-
-                // Übergangsschicht
-                for (int y = STONE_TOP_Y + 1; y <= SUBSURFACE_TOP_Y; y++) {
-                    chunkData.setBlock(x, y, z, biome.getSubSurfaceBlock());
-                }
-
-                // Oberfläche
-                chunkData.setBlock(x, SURFACE_Y, z, biome.getSurfaceBlock());
             }
         }
-    }
-
-    @Override
-    public boolean shouldGenerateCaves() {
-        // Höhlen sorgen für sinnvolle Dungeon-Verstecke im Untergrund
-        return true;
-    }
-
-    @Override
-    public boolean shouldGenerateNoise() {
-        return false;
-    }
-
-    @Override
-    public boolean shouldGenerateDecorations() {
-        return false; // wir übernehmen Vegetation selbst über die Populatoren
-    }
-
-    @Override
-    public boolean shouldGenerateMobs() {
-        return true;
-    }
-
-    @Override
-    public boolean shouldGenerateStructures() {
-        return false; // keine Vanilla-Strukturen, wir haben eigene Dungeons
-    }
-
-    @Override
-    public @NotNull List<BlockPopulator> getDefaultPopulators(@NotNull org.bukkit.World world) {
-        return List.of(
-                new TreePopulator(biomeZoneSize),
-                new DungeonPopulator()
-        );
     }
 }
